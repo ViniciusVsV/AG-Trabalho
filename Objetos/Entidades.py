f270f677752c74be4c999a307f371a6885d2b4be
ViniciusVsV <viniciusvsv193@gmail.com>
2025-06-05 10:51:26 -0300

    Mantendo padrão de nomenclaturas para em Disciplina e Turma

--- Objetos/Entidades.py
+++ Objetos/Entidades.py
@@ -9,54 +9,56 @@ class Disciplina:
         sigla (str): Sigla da disciplina.
         nome (str): Nome da disciplina.
         curso (str): Curso ao qual a disciplina pertence.
-        categoria (str): Categoria da disciplina (obrigatória, optativa, etc.).
-        semestre (int): Semestre em que a disciplina é oferecida.
+        categoria (str): Categoria da disciplina (obrigatória, optativa, equivalente).
+        periodo (int): Periodo do curso pretendido para a conclusão da disciplina.
         anualidade (bool): Indica se a disciplina é anual.
-        carga_horaria (int): Carga horária da disciplina.
-        pre_requisitos (PreRequisito): Pré-requisitos da disciplina.
+        cargaHoraria (int): Carga horária da disciplina.
+        preRequisitos (PreRequisito): Pré-requisitos da disciplina.
         equivalentes (Equivalente): Disciplinas equivalentes. 
-        correquisitos (set): Conjunto de disciplinas que devem ser cursadas simultaneamente.
-        peso (float): Peso da disciplina, usado para cálculo de média ponderada.
-        turmas (list): Lista de horários das turmas disponíveis para a disciplina.
+        correquisitos (set): Correquisitos da disciplina.
+        peso (float): Peso da disciplina.
+        turmas (list): Lista de turmas da disciplina.
     """
-    def __init__(self, sigla: str, nome: str, curso: str, categoria: str, semestre: int, anualidade: str, carga_horaria: int, pre_requisitos: str = '-', equivalentes: str = '-', correquisito: str = '-', peso: float = 0.0):
+    def __init__(self, sigla: str, nome: str, curso: str, categoria: str, periodo: int, anualidade: str, cargaHoraria: int, preRequisitos: str = '-', equivalentes: str = '-', correquisito: str = '-', peso: float = 0.0):
         self.sigla = sigla
         self.nome = nome
         self.curso = curso
         self.categoria = categoria
-        self.semestre = semestre
+        self.periodo = periodo
         self.anualidade = anualidade == 'SIM'
-        self.carga_horaria = carga_horaria
+        self.cargaHorario = cargaHoraria
 
-        self.pre_requisitos = PreRequisito(pre_requisitos)
+        self.preRequisitos = PreRequisito(preRequisitos)
         self.equivalentes = Equivalente(equivalentes)
-
         self.correquisitos = set(corr.strip() for corr in correquisito.split(',')) if correquisito and correquisito != '-' else set()
+        
         self.peso = peso if categoria == "OBRIGATORIA" else 0.0
 
         self.turmas: list[(int, str, int)] = []
 
-    def AdicionaTurmas(self, nro_turma: int, horario: str, semestre: int, ):
+    def AdicionaTurmas(self, numeroTurma: int, horario: str, semestre: int, ):
         """
-        Adiciona um horário de turma à disciplina.
+        Adiciona uma turma à disciplina.
 
         Args:
-            nro_turma (int): Número da turma a ser adicionada.
+            numeroTurma (int): Número da turma a ser adicionada.
             horario (str): Horário da turma a ser adicionada.
-            semestre (int): Semestre do ano da turma a ser adicionada.
+            semestre (int): Semestre do ano em que a turma a ser adicionada é ofertada (1, 2).
         """
-        self.turmas.append((nro_turma, horario, semestre))
+        self.turmas.append((numeroTurma, horario, semestre))
 
     def CriaTurmas(self) -> list['Turma']:
         """
-        Cria uma lista de objetos Turma a partir dos horários da disciplina.
+        Cria uma lista de objetos Turma a partir das turmas da disciplina.
 
         Returns:
             list[Turma]: Lista de objetos Turma criados.
         """
         turmas = []
-        for (nro_turma, horario, semestre) in self.turmas:
-            turmas.append(Turma(self, nro_turma=nro_turma, horarios=horario, semestre=semestre, peso=self.peso))
+
+        for (numeroTurma, horario, semestre) in self.turmas:
+            turmas.append(Turma(self, numeroTurma, horario, semestre, self.peso))
+        
         return turmas
 
     def isPreRequisito(self, outro: 'Disciplina') -> bool:
@@ -71,7 +73,7 @@ class Disciplina:
 
         return self.pre_requisitos.contem(outro.sigla)
     
-    def atendePreRequisitos(self, disciplinasCumpridas: set[str]) -> bool:
+    def VerificaPreRequisitos(self, disciplinasCumpridas: set[str]) -> bool:
         """
         Verifica se os pré-requisitos da disciplina foram atendidos.
 
@@ -82,7 +84,7 @@ class Disciplina:
         """
         return self.pre_requisitos.verifica(disciplinasCumpridas)
     
-    def atendeEquivalencia(self, disciplinasEquivalentes: set[str]) -> bool:
+    def VerificaEquivalencia(self, disciplinasEquivalentes: set[str]) -> bool:
         """
         Verifica se alguma equivalência da disciplina foi atendida.
 
@@ -107,15 +109,16 @@ class Disciplina:
         return f"Disciplina(sigla={self.sigla}, nome={self.nome})"
 
 class Turma:
-    def __init__(self, disciplina: 'Disciplina', nro_turma: int, horarios: str, semestre: int, peso: float = 0.0):
+    def __init__(self, disciplina: 'Disciplina', numeroTurma: int, horario: str, semestre: int, peso: float = 0.0):
         self.disciplina = disciplina
-        self.nro_turma = nro_turma
-        self.horarios = Horario(horarios)
+
+        self.numeroTurma = numeroTurma
+        self.horario = Horario(horario)
         self.semestre = semestre
 
         self.peso = peso
 
-    def verificaHorarioConflitante(self, outro: 'Turma') -> bool:
+    def VerificaHorarioConflitante(self, outro: 'Turma') -> bool:
         """
         Verifica se há conflito de horários entre duas turmas.
 
@@ -128,14 +131,14 @@ class Turma:
         return self.horarios.isConflitante(outro.horarios)
     
     @property
-    def codigo(self) -> str:
+    def sigla(self) -> str:
         """
-        Retorna o código da disciplina associada à turma.
+        Retorna a sigla da disciplina associada à turma.
 
         Returns:
-            str: Código da disciplina.
+            str: Sigla da disciplina.
         """
-        return self.disciplina.codigo
+        return self.disciplina.sigla
 
     # Só para garantir que a verificação de igualdade funcione corretamente
     # Estava dando problema por causa que comparada as referências de objetos
